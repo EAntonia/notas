3c2049969e713ece8602de56c8a0010220290d1e
Edydesantiago <153242879+Edydesantiago@users.noreply.github.com>
2024-01-08 21:34:20 -0600

    Conexion a MongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const url = process.env.MONGODB_URL;
-//const url = "mongodb+srv://m23450794:<-notas.2023>@cluster0.ojebdqv.mongodb.net/?retryWrites=true&w=majority"
 
-
-mongoose.connect('mongodb://127.0.0.1/notasdb')
-//mongoose.connect(url)
+mongoose.connect(url)
         .then( db=> console.log("Base de datos conectada"))
         .catch( err=> console.log(err));
